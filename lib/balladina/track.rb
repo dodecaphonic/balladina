8c218eb645c2f3f740303caadfa77ba6a3bcac74
Vitor Capela <dodecaphonic@gmail.com>
2014-03-14 11:09:03 -0300

    Add some UI styling.

--- lib/balladina/track.rb
+++ lib/balladina/track.rb
@@ -18,13 +18,13 @@ module Balladina
 
     def start_recording
       @recorder = creates_recorders.new_link(Actor.current, socket)
-      info "Track: starting to record"
+      info "Track \##{id}: starting to record"
       @recorder.async.record
     end
 
     def stop_recording
       @recorder.terminate if @recorder
-      info "Track: stopping recording"
+      info "Track \##{id}: stopping recording"
       @recorder = nil
     end
 
@@ -34,7 +34,6 @@ module Balladina
 
     def on_chunk(chunk_path)
       chunks << chunk_path
-      info "NEW CHUNK. CHUNKS ARE: #{chunks.inspect}"
     end
 
     def recorder_died(actor, reason)
